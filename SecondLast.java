2158cb77f4f62d2d51d25379ddce59a5f742304b
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 00:43:48 +0530

    Create SecondLast.java

---
+++ SecondLast.java
@@ -0,0 +1,8 @@
+ class SecondLast {
+    public static void main(String[] args) {
+        int num =123;
+        int result = ((num / 10)%10);
+        
+        System.out.println("result:"+ result);
+    }
+}
