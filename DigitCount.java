bedd48b94353738a77fe933c202ff1f938708a1e
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 00:48:18 +0530

    Create DigitCount.java

---
+++ DigitCount.java
@@ -0,0 +1,11 @@
+class DigitCount {
+    public static void main(String[] args) {
+        int num = 123;int count =0;
+        while (num >0)
+        {
+        num = num /10;
+        count ++;
+        }
+        System.out.println("Digit Count :"+ count);
+    }
+}
