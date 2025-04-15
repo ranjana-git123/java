a2d6d7b786a6d023cb9ccf858c8b492c320d5131
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 00:47:34 +0530

    Create SumOfDigit.java

---
+++ SumOfDigit.java
@@ -0,0 +1,12 @@
+class SumOfDigit {
+    public static void main(String[] args) {
+        int num = 123;int sum =0;int digit= 0;
+        while (num >0)
+        {
+        digit = num % 10;
+        sum = sum + digit;
+        num = num /10;
+        }
+        System.out.println("Sum of the digits are :"+ sum);
+    }
+}
