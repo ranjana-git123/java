cde7d62bd82c713ba9a912334bfa1bc6e8660983
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 00:45:59 +0530

    Create SumofLastDigit.java

---
+++ SumofLastDigit.java
@@ -0,0 +1,10 @@
+class SumofLastDigit {
+    public static void main(String[] args) {
+        int a = 54 ; int b =59;
+        int A  = a % 10;
+        int B  = b % 10;
+        int C  = A + B ;
+        
+        System.out.println("result:"+ C);
+    }
+}
