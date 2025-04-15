d05e400ac5213435dba240c6e8b0bd3806b9109e
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 00:49:25 +0530

    Create FindBigNumber.java

---
+++ FindBigNumber.java
@@ -0,0 +1,13 @@
+class FindBigNumber {
+    public static void main(String[] args) {
+        int num = 123;int big=0;int a;
+        while (num >0)
+        {
+        a = num %10;
+        if (a > big)
+        big = a;
+        num =num/10;
+        }
+        System.out.println(big);
+    }
+}
