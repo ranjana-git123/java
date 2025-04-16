d0bd94743292da0c871e479fcbaf13239ccbebb5
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 23:03:22 +0530

    Create VowelConsonant.java

---
+++ VowelConsonant.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner Sc=new Scanner(System.in);
+        System.out.println("Enter any character: ");
+        
+        char ch = Sc.next().toLowerCase().charAt(0);
+       
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            System.out.println(ch + " is a Vowel.");
+        } else if (ch >= 'a' && ch <= 'z') {
+            System.out.println(ch + " is a Consonant.");
+        } else {
+            System.out.println("Not a valid alphabet.");
+        }
+        }
+    }
