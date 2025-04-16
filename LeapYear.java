254c3b2a148337b77fc9b02bb6b9e6055a6313a0
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 23:06:19 +0530

    Create LeapYear.java

---
+++ LeapYear.java
@@ -0,0 +1,15 @@
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a year: ");
+        int year = input.nextInt();
+
+        // Leap year condition
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " is a Leap Year.");
+        } else {
+            System.out.println(year + " is NOT a Leap Year.");
+        }
+    }
+}
