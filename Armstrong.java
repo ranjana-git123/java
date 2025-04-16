c6a2e7e500f8e46268763a2e33abb62602ff64cc
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-16 22:22:20 +0530

    Create Armstrong.java

---
+++ Armstrong.java
@@ -0,0 +1,18 @@
+class Armstrong {
+    public static void main(String[] args) {
+        int n=153;int A=0; int B=0;int C=n;int D=0;
+        while(n>0){
+            A=n%10;
+            B=A*A*A;
+            D=D+B;
+            n=n/10;
+        }
+        System.out.println(D);
+        if(C==D){
+            System.out.println("YES IT IS A ARMSTRONG NUMBER");
+        }
+        else{
+            System.out.println("NO IT IS NOT A ARMSTRONG NUMBER");
+        }
+    }
+}
