a35c06b6b307d6defa2e2249b0a530d98ee4d6ce
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-21 00:20:27 +0530

    Create Eliminate5.java

---
+++ Eliminate5.java
@@ -0,0 +1,26 @@
+public class Eliminate5 {
+    public static void main(String[] args) {
+        int n=0;
+        while(n<10) {
+            n++;
+            if(n==5)
+            continue;
+            System.out.println(n);
+         
+            
+        }
+        
+    }
+}
+
+
+o/p
+1
+2
+3
+4
+6
+7
+8
+9
+10
