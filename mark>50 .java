30c62b6277d9e7e7b88824038ff837f360dabb22
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-04-21 01:12:59 +0530

    Create mark>50 .java

---
+++ mark>50 .java
@@ -0,0 +1,19 @@
+public class mark>50  {
+    public static void main(String[] args) {
+        int[] mark = {20,35,80,14,100,56};
+        int i = 0;
+
+        while (i < mark.length) {
+            if (mark[i]>50)
+            
+            System.out.println(mark[i]);
+            i++;
+        }
+        
+    }
+}
+
+// o/p
+// 80
+// 100
+// 56
