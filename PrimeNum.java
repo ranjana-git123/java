93e9ede1f1d931467c4c8cbf16f4ca0b85b6830b
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-06-21 21:26:37 +0530

    Update PrimeNum.java

--- PrimeNum.java
+++ PrimeNum.java
@@ -1,17 +1,19 @@
 class PrimeNum {
     public static void main(String[] args) {
-        int n=7;int count=0;
-        for(int i=0;i<=n;i++)
-        {
-            if(i % n ==0)
-            count++;
+        int n = 9;
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0)
+                count++;
         }
-        
-        if(count==2){
-            System.out.println("YES it is a prime ");
-        }
-        else{
-            System.out.println("NO it is not a prime");
+
+        if (count == 2) {
+            System.out.println("YES, it is a prime");
+        } else {
+            System.out.println("NO, it is not a prime");
         }
     }
 }
+
+// NO, it is not a prime
