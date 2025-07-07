fa4a7f2aa39ade4e572f6a45d32c7526630c5ce0
ranjana-git123 <155462124+ranjana-git123@users.noreply.github.com>
2025-07-08 01:20:17 +0530

    Create squareWall.java

---
+++ squareWall.java
@@ -0,0 +1,20 @@
+class Solution {
+    public static void squareWall(int s) {
+        // Write your code here
+        String row ="* ".repeat(s);
+        for(int i=1;i<=s;i++){
+            
+             System.out.println(row);
+            }
+        }
+    }
+
+
+
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+// * * * * *
+
+
